f7af39ee2827c3a71cc847a1fa97f9d2e20434d2
Dongri Jin <dongrium@gmail.com>
2025-12-28 22:30:43 +0900

    coins v0.1.2

--- coins.rb
+++ coins.rb
@@ -1,7 +1,7 @@
 class Coins < Formula
   desc "Terminal-based cryptocurrency price tracker"
   homepage "https://github.com/dongri/coins"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   on_macos do

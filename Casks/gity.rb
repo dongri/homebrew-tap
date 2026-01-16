9dd919a983d9fe5ff73fd8ab7559625e4d41b1e4
Dongri Jin <dongrium@gmail.com>
2026-01-16 22:43:46 +0900

    Fix sha256

--- Casks/gity.rb
+++ Casks/gity.rb
@@ -1,6 +1,6 @@
 cask "gity" do
   version "1.0.1"
-  sha256 "SHA256_OF_YOUR_DMG_FILE" # shasum -a 256 GitY.dmg の結果をここに貼る
+  sha256 "511ea9521ed5d242bc0c17f104a02e112a4843a75dc0fef4219fd48cce494e13" # shasum -a 256 GitY.dmg の結果をここに貼る
 
   url "https://github.com/dongri/gity/releases/download/v#{version}/GitY.dmg"
   name "GitY"

70eb6f32ed421a3b843b8946a048542f8b9da8a9
Dongri Jin <dongrium@gmail.com>
2026-01-16 22:41:59 +0900

    Fix version

--- Casks/gity.rb
+++ Casks/gity.rb
@@ -1,5 +1,5 @@
 cask "gity" do
-  version "v1.0.1"
+  version "1.0.1"
   sha256 "SHA256_OF_YOUR_DMG_FILE" # shasum -a 256 GitY.dmg の結果をここに貼る
 
   url "https://github.com/dongri/gity/releases/download/v#{version}/GitY.dmg"
